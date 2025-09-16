62471d4bcaa84807c480c5bdccdc77046b03f039
Олег Бондар <lluccija18@gmail.com>
2025-09-16 20:54:21 +0300

    Add project files.

---
+++ Project1/Project1.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <random>
+#include <algorithm>
+#include "RandomByFrequency.h"
+
+using namespace std;
+
+int main()
+{
+
+	return 0;
+}
---
+++ Project1/RandomByFrequency.cpp
@@ -0,0 +1,1 @@
+#include "RandomByFrequency.h"
